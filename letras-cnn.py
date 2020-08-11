c061b92d0451208f148518c90125e20b8216d894
vinihvalentim <63162887+vinihvalentim@users.noreply.github.com>
2020-08-11 14:44:51 -0300

    Update letras-cnn.py

--- letras-cnn.py
+++ letras-cnn.py
@@ -60,7 +60,7 @@ print(x_test.shape)
 def define_model():
     model = Sequential()
     model.add(Conv2D(filters=128, kernel_size=(5,5), padding = 'same', activation='relu',\
-                             input_shape=(HEIGHT, WIDTH,1)))
+                             input_shape=(28, 28,1)))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
     model.add(Conv2D(filters=64, kernel_size=(3,3) , padding = 'same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
